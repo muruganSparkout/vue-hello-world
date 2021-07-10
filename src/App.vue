7d04600fd0ba25af69342a06d9a3dcc842c42c09
Murugan P <murugan@sparkouttech.com>
2021-07-10 18:25:35 +0530

    v-once and v-on usage

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,11 @@
 <template>
   <h1> Welcome to {{title}} </h1>
-
-  <!-- <div v-html="tutorial"> </div>
-  <h3 v-bind:id="bindId" v-bind:class="bindClass">This is third line</h3> -->
-
-  <!-- v-if condition -->
-  <!-- <h2 v-if="number<5"> {{number}} is less than 5</h2>
-  <h2 v-else-if="number>5"> {{number}} is greater than 5</h2>
-  <h2 v-else > {{number}} is equal to 5</h2> -->
-
-  <!-- v-show -->
-  <!-- <h2 v-show="number==5"> {{number}} is {{ number==5 ? 'displaying' : 'hidden' }} </h2> -->
-
-  <!-- loops -->
-  <ul>
-    <li v-for="(fruit,index) in Fruits" :key="fruit" >
-      <p v-if="index>0">{{ fruit }} {{index}}</p>
-    </li>
-  </ul>
-  <!-- object listing -->
-  <!-- <ul id="v-for-object" class="demo">
-    <li v-for="(value,key) in myObject" :key="key">
-      {{ value }}
-    </li>
-  </ul> -->
+  <h2>This counter number: {{counter}} </h2>
+  <div v-once>
+    {{ counter }}
+  </div>
+  
+  <button v-on:click="buttonSubmit">Button</button>
 
 </template>
 
@@ -31,22 +13,25 @@
 
   export default {
     name: 'App',
-    data(){
-      return {
-        title:'vue 3 tutorial',
-        // tutorial:"This is vue js basic tutorial",
-        // bindId:"SectionId",
-        // bindClass:'underline',
-        // number: 5,
-        Fruits: ['mango','apple','grapes'],
-        myObject: {
-          title: 'How to do lists in Vue',
-          author: 'Jane Doe',
-          publishedAt: '2016-04-10'
+      data(){
+        return {
+          title:'vue 3 tutorial',
+          counter: 1,
+        }
+      },
+      mounted() {
+        setInterval(() => {
+            this.counter++
+          }, 1000)
+      },
+      methods:{
+        buttonSubmit(){
+          alert('ok')
         }
       }
+
     }
-  }
+    
 </script>
 
 <style>
