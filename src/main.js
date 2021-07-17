769711b7d2cde1f93a62b69a091db33bdfc3734c
Murugan P <murugan@sparkouttech.com>
2021-07-17 20:02:32 +0530

    add analytics

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueAnalytics from 'vue-analytics';
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(VueAnalytics,{
+    id: 'UA-12345678-1',
+    router
+}).mount('#app')
